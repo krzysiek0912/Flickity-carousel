4cda0a656ba0a0c71d1a91203a4c8bd88e1b9fdd
Krzysztof Branecki <krzysztof@branecki.net>
2019-07-03 14:16:10 +0200

    Add template to Flickity carousel

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 const elem = document.querySelector(".main-carousel"),
   buttonFirst = document.querySelector(".button-first"),
-  progressBar = document.querySelector(".progress-bar");
+  progressBar = document.querySelector(".progress-bar"),
+  carousel = document.querySelector(".main-carousel");
+
+let sliderItemTemplate = document.querySelector("#template-slider-item")
+    .innerHTML,
+  sliderItems = "";
+
+Mustache.parse(sliderItemTemplate);
+
+for (var i = 0; i < sliderData.length; i++) {
+  sliderItems += Mustache.render(sliderItemTemplate, {
+    ...sliderData[i],
+    id: i
+  });
+}
+
+carousel.insertAdjacentHTML("beforeend", sliderItems);
 
 var flkty = new Flickity(elem, {
   // options
