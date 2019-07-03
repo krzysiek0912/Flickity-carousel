79ab27b9ea62a67aa70759db897e1c6febb4f9f3
Krzysztof Branecki <krzysztof@branecki.net>
2019-07-03 17:33:10 +0200

    Off center map on click marker

--- js/script.js
+++ js/script.js
@@ -34,14 +34,11 @@ flkty.on("scroll", function(progress) {
   progress = Math.max(0, Math.min(1, progress));
   progressBar.style.width = progress * 100 + "%";
 });
-var infos = document.getElementById("infos");
+
 //Google Map
 window.initMap = function() {
   // The location of Uluru
   var uluru = sliderData[0].coords;
-  var sydney = { lat: -33.874237, lng: 151.198517 };
-  var coords2 = { lat: -25.363, lng: 134.044 };
-  var coords3 = { lat: -25.363, lng: 137.044 };
 
   // The map, centered at Uluru
   var map = new google.maps.Map(document.getElementById("map"), {
@@ -55,63 +52,17 @@ window.initMap = function() {
       map: map,
       i: i
     });
-    // console.log(i);
     markers.addListener("click", function() {
       flkty.select(markers.i, false, true);
-
-      //   console.log(markers.i);
     });
-    // console.log(marker);
-    //     infos.innerHTML = "You clicked markerThree";
-    //   });
   }
 
-  //   var markerOne = new google.maps.Marker({
-  //     position: center,
-  //     map: map
-  //   });
-  //   // The marker, positioned at Uluru
-
-  //   var marker = new google.maps.Marker({ position: uluru, map: map });
-
-  //   markerOne.addListener("click", function() {
-  //     // Wewnątrz funcji wpisujemy kod, który ma się wykonać po kliknięciu markera. W tym przykładzie wyświetlimy tekst na stronie.
-  //     infos.innerHTML = "You clicked markerOne";
-  //   });
-  //   var markerTwo = new google.maps.Marker({
-  //     position: coords2,
-  //     map: map
-  //   });
-
-  //   markerTwo.addListener("click", function() {
-  //     infos.innerHTML = "You clicked markerTwo";
-  //   });
-
-  //   var markerThree = new google.maps.Marker({
-  //     position: coords3,
-  //     map: map
-  //   });
-
-  //   markerThree.addListener("click", function() {
-  //     infos.innerHTML = "You clicked markerThree";
-  //   });
-
-  //   document
-  //     .getElementById("center-map")
-  //     .addEventListener("click", function(event) {
-  //       event.preventDefault();
-  //       // Najpierw wykorzystujemy metodę panTo w obiekcie map do przesunięcia współrzędnych mapy:
-  //       map.panTo(sydney);
-
-  //       // A następnie zmieniamy powiększenie mapy:
-  //       map.setZoom(10);
-  //     });
-  //   document
-  //     .getElementById("center-smooth")
-  //     .addEventListener("click", function(event) {
-  //       event.preventDefault();
-  //       smoothPanAndZoom(map, 7, uluru);
-  //     });
+  flkty.on("change", function(index) {
+    event.preventDefault();
+    if (event.target.localName !== "area")
+      smoothPanAndZoom(map, 7, sliderData[index].coords);
+    //   console.log("Flickity change " + index);
+  });
 }; //End Map
 
 var smoothPanAndZoom = function(map, zoom, coords) {
